392bc30a3f3506ad87c9d8f21cb247b2d0f5437c
obaylor3 <owenbaylor@icloud.com>
2017-06-27 10:26:34 -0400

    Movie Recomendation Bot

--- P3 Recommendations.py
+++ P3 Recommendations.py
@@ -7,30 +7,32 @@ import numpy as np
 
 def findSimilar(iLike, userLikes):
     # Create an And similarity
-    similarityAnd = 0 # replace 0 with the correct code
+    similarityAnd = iLike * userLikes # replace 0 with the correct code
     # Create a per user sum
-    similarityAndSum = 0 # replace 0 with the correct code
+    similarityAndSum = np.sum(similarityAnd,axis=1) # replace 0 with the correct code
     # Create an Or similarity
-    userSimilarityOr = 0 # replace 0 with the correct code
+    userSimilarityOr = iLike + userLikes - similarityAnd # replace 0 with the correct code
     
     # Calculate the similarity
-    userSimilarity = 0 # replace 0 with the correct code to calculate the Jaccard Index for each user
+    userSimilarity = similarityAndSum / userSimilarityOr.sum(axis = 1) # replace 0 with the correct code to calculate the Jaccard Index for each user
     
     # Make the most similar user has a new like that the previous user did not have
     # I used a while loop.
     # You can "get rid" of a user that is most similar, but doesn't have any new likes
     # by setting the userSimilarity for them to 0
     # When you get the index, save it in the variable maxIndex
-    
+    while  :
+
     # Print the max similarity number (most times this is something like 0.17
-    
+    print (userSimilarity)
     # Return the index of the user which is the best match
     return maxIndex
     
 def printMovie(id):
+
     # Print the id of the movie and the name.  This should look something like
     # "    - 430: Duck Soup (1933)" if the id is 430 and the name is Duck Soup (1933)
-    print(0) # replace 0 with the correct code
+    print(printMovie) # replace 0 with the correct code
 
 def processLikes(iLike):
     print("\n\nSince you like:")
@@ -41,7 +43,11 @@ def processLikes(iLike):
     # Convert iLike into an array of 0's and 1's which matches the array for other users
     # It should have one column for each movie (just like the userLikes array)
     # Start with all zeros, then fill in a 1 for each movie the user likes
-    ilikeNp = 0    # replace 0 with the code to make the array of zeros
+    ilikeNp = np.zeros(maxMovie)
+    for id in iLike:
+        iLike[id] = 1
+    # replace 0 with the code to make the
+    #array of zeros
     # You'll need a few more lines of code to fill in the 1's as needed
 
     # Find the most similar user
@@ -49,11 +55,13 @@ def processLikes(iLike):
     print("\nYou might like: ")
     # Find the indexes of the values that are ones
     # https://stackoverflow.com/a/17568803/3854385 (Note: You don't want it to be a list, but you do want to flatten it.)
-    recLikes = 0 # replace 0 with the needed code
-
+    recLikes = np.argwhere(userLikes[1, :] == 1) # replace 0 with the needed code
+    print(recLikes.flatten())
     # For each item the similar user likes that the person didn't already say they liked
     # print the movie name using printMovie (you'll also need a for loop and an if statement)
-
+for:
+    if:
+        print(printMovie)
 ########################################################
 # Begin Phase 1
 ########################################################
@@ -166,8 +174,9 @@ userLikes = np.zeros((maxUser, maxMovie))
 # Go through all the rows of the movie data.
 # If the user rated a movie as 4 or 5 set userLikes to 1 for that user and movie
 # Note: You'll need a for loop and an if statement
-for maxUser in userLikes:
-    if
+for  in  :
+    if  > 3:
+        userLikes = 1
 
 ########################################################
 # At this point, go back up to the top and fill in the
@@ -179,7 +188,7 @@ for maxUser in userLikes:
 iLike = [655, 315, 66, 96, 194, 172]
 processLikes(iLike)
 
-# What if it's an exact match? We should return the next closest match
+# What if it's an exact match? We should return the next closest msdaatch
 # Second sample case
 # User Similiarity: 0.172413793103
 iLike = [ 79,  96,  98, 168, 173, 176,194, 318, 357, 427, 603]
